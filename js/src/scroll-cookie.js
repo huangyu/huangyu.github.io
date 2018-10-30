ba8d0cc20f1fdeb0ce89b60b8063724500e530a9
huangyu <poxiaohy@gmail.com>
2018-10-30 15:01:48 +0800

    Site updated: 2018-10-30 15:01:48

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 18 2018 15:47:54 GMT+0800 (CST)
+// build time:Tue Oct 30 2018 15:01:42 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
